4d7674687bf617cac54f16e2c255d36ffd60fc1f
Facundo <facu03@gmail.com>
2022-07-16 06:33:40 -0300

    typedjs

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,3 @@ import { SceneManager } from './utils/SceneManager';
 SceneManager.initialize();
 
 SceneManager.changeScene(new LoaderScene());
-
--- src/scenes/Config.ts
+++ src/scenes/Config.ts
@@ -1,5 +1,7 @@
-import { Texture} from "pixi.js";
-// import { GenericText } from "../ui/GenericText";
+import { sound } from "@pixi/sound";
+import {  Texture } from "pixi.js";
+import { Tween } from "tweedle.js";
+// import Typed from "typed.js";
 import { PointButton } from "../ui/PointButton";
 import { SceneBase } from "../utils/SceneBase";
 import { SceneManager } from "../utils/SceneManager";
@@ -8,22 +10,24 @@ import { GameStartScene } from "./GameStartScene";
 
 export class Config extends SceneBase {
 
-    public update(): void {
-    }
-
+    private buttonText: PointButton;
     private buttonMouse: PointButton;
+
+    public mostrarEscrito: boolean = false;
 
     constructor() {
         super();
 
-        // const cuadrodetexto = new GenericText("Como estas? Este es un texto de para probar."
-        //     + "\n" +
-        //     "Si no te gusta esto, puedes volver al inicio."
-        //     + "\n" +
-        //     "Si te gusta el juego, puedes continuar con el juego."
-        //     + "\n" +
-        //     "Y si no te gusta el juego, podes salir del todo.", "Supertext02", 73);
-        // this.addChild(cuadrodetexto);
+        this.buttonText = new PointButton(Texture.from("BACK.png"),
+            Texture.from("BACK hundido.png"),
+            Texture.from("BACK.png"));
+        this.buttonText.x = 20
+        this.buttonText.y = 50
+        this.buttonText.scale.x = 0.5;
+        this.buttonText.scale.y = 0.5;
+        this.buttonText.on("pointerClick", this.onButtonText, this);
+
+        
 
         { /* START:                 COMO QUEDÓ EL BOTON DE START AL FINAL*/
             this.buttonMouse = new PointButton(Texture.from("BACK.png"),
@@ -37,9 +41,14 @@ export class Config extends SceneBase {
 
 
             { // ADD.CHILD:             AGREGANDO TODO CON ADDCHILDS
-                this.addChild(this.buttonMouse);
+                this.addChild(this.buttonMouse, this.buttonText);
             }
         }
+    }
+
+    public update(): void {
+
+        // console.log(this.aux.innerHTML);
     }
 
     //BUTTON.TS            HACER FUNCIONAR EL NUEVO BOTÓN  
@@ -47,4 +56,86 @@ export class Config extends SceneBase {
         console.log("Apreté volver", this);
         SceneManager.changeScene(new GameStartScene());
     }
+
+    private onButtonText() {
+        this.mostrarEscrito = true;
+        console.log("apreté el texto")
+        console.log(this.mostrarEscrito);
+        const texto: string = "Hola, soy un texto";
+        const subtexto = texto.substring(0, texto.length);
+
+        if (this.mostrarEscrito=true) {
+            console.log("mostrando escrito");
+            
+            for (let i = 0; i < subtexto.length; i++) {
+                new Tween(subtexto[i])
+                .from({ x: 300 })
+                .to({ x: 700 }, 3000)
+                .start()
+                .onComplete(this.Waiting.bind(this));
+                console.log(subtexto[i]);
+                sound.play("PotionSound1");
+            }
+        }}
+
+        // codigo para escribir letras de un texto una por una
+
+        //         const textoescrito = new Text(subtexto[i], {
+        //             fontFamily: "Arial",
+        //             fontSize: "50px",
+        //             fill: "white",
+        //             align: "center",
+        //             wordWrap: true,
+        //             lineHeight: 50,
+        //             padding: 10,
+        //             stroke: "black",
+        //             strokeThickness: 5,
+        //             dropShadow: true,
+        //             dropShadowColor: "black",
+        //             dropShadowBlur: 4,
+        //             dropShadowAngle: Math.PI / 6,
+        //             dropShadowDistance: 6,
+        //             wordWrapWidth: this.width - 100,
+        //         });
+        //         this.addChild(textoescrito);
+        //         textoescrito.position.set(200,200);
+        //     }
+        // }
+    
+
+    private soundText() {
+        sound.play("PotionSound1");
+    }
+
+    private Waiting(): void {
+        console.log("waiting");  
+        new Tween(this.buttonText).to({}, 3000).start();
+        this.soundText();
+        
+    }
+
+
 }
+
+    // private aux: HTMLDivElement;
+
+
+// let str:string= "Hola, soy un texto que va a ser mostrado en pantalla.\n"
+// let currentText = new Tween(this.str.substring(0, str.length-1)).to({},3000).start();
+// this.textOnScreen = new Text(`${currentText}`, { fontSize: 40, fontFamily: ("Arial") });
+// this.addChild(this.textOnScreen);
+
+// this.addChild(str);
+
+        // this.aux= document.createElement("div");
+        // const text = new Typed(this.aux, {
+        //     strings: [subtexto],
+        //     typeSpeed: 50,
+        //     loop: false,
+        //     showCursor: false,
+        //     startDelay: 1000,
+        //     onStringTyped: () => {
+        //         console.log("String typed");
+        //     }
+        //  });
+        //  text.start();
